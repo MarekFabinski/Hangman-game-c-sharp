8df0b6f299b094d3e68b4bb13dcbf682fdb5a5aa
MarekFabinski <48487492+MarekFabinski@users.noreply.github.com>
2019-05-21 20:06:35 +0200

    Add files via upload

---
+++ hangman/hangman/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "_";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(78, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 43);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "_";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(122, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 43);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "_";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(166, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 43);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "_";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(210, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 43);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "_";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(254, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 43);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "_";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(298, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 43);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "_";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(41, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(295, 100);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Sprawdź";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(342, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(297, 258);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(294, 25);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 309);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "The Hangman";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ hangman/hangman/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hangman
+{
+    public partial class Form1 : Form
+    {
+        String word;
+        int missed = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            Randomize_word();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (missed < 12)
+            {
+                string letter = textBox1.Text;
+                bool guessed = false;
+                int where = 0;
+                for (int i = 0; i < 6; i++)
+                {
+                    if (Convert.ToString(word[i]) == letter)
+                    {
+                        guessed = true;
+                        where = i;
+                    }
+                    if (where == 1) { label2.Text = letter; }
+                    if (where == 2) { label3.Text = letter; }
+                    if (where == 3) { label4.Text = letter; }
+                    if (where == 4) { label5.Text = letter; }
+                    if (where == 5) { label6.Text = letter; }
+                }
+                if (guessed == false)
+                {
+                    missed++;
+                    if (missed == 1) { pictureBox1.Image = hangman.Properties.Resources._1; }
+                    if (missed == 2) { pictureBox1.Image = hangman.Properties.Resources._2; }
+                    if (missed == 3) { pictureBox1.Image = hangman.Properties.Resources._3; }
+                    if (missed == 4) { pictureBox1.Image = hangman.Properties.Resources._4; }
+                    if (missed == 5) { pictureBox1.Image = hangman.Properties.Resources._5; }
+                    if (missed == 6) { pictureBox1.Image = hangman.Properties.Resources._6; }
+                    if (missed == 7) { pictureBox1.Image = hangman.Properties.Resources._7; }
+                    if (missed == 8) { pictureBox1.Image = hangman.Properties.Resources._8; }
+                    if (missed == 9) { pictureBox1.Image = hangman.Properties.Resources._9; }
+                    if (missed == 10) { pictureBox1.Image = hangman.Properties.Resources._10; }
+                    if (missed == 11) { pictureBox1.Image = hangman.Properties.Resources._11; }
+                    if (missed == 12) {
+                        pictureBox1.Image = hangman.Properties.Resources._12;
+                        button2.Visible = true;
+                    }
+                }
+                Win();
+            }
+            else
+            {
+                pictureBox1.Image = hangman.Properties.Resources.lose;
+            }
+        }
+
+        private void Randomize_word()
+        {
+            string[] words = { "ability", "banking", "certain", "decline", "freedom", "instead", "massive", "perhaps" };
+            int word_count = words.Length;
+            Random gen = new Random();
+            int word_index = gen.Next(0, word_count);
+            word = words[word_index];
+            label1.Text = Convert.ToString(word[0]);
+            label7.Text = Convert.ToString(word[6]);
+        }
+
+        private void Win()
+        {
+            if (label2.Text != "_")
+            {
+                if (label3.Text != "_")
+                {
+                    if (label4.Text != "_")
+                    {
+                        if (label5.Text != "_")
+                        {
+                            if (label6.Text != "_")
+                            {
+                                pictureBox1.Image = hangman.Properties.Resources.win;
+                                button2.Visible = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Randomize_word();
+            label2.Text = "_";
+            label3.Text = "_";
+            label4.Text = "_";
+            label5.Text = "_";
+            label6.Text = "_";
+            missed = 0;
+            button2.Visible = false;
+            pictureBox1.Image = null;
+        }
+    }
+}
